2b9d44a098c91c7592256b1546afd05a06d2c39d
meryem2137 <116557318+meryem2137@users.noreply.github.com>
2023-01-25 17:51:55 +0300

    Add files via upload

---
+++ basamaksayılarıtoplamı.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class basamaksayılarıtoplamı {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.print("Sayı giriniz: ");
+        number= input.nextInt();
+        int busnumber=0;
+        int tempnumber=number;
+        int basvalue;
+
+        while(tempnumber !=0){
+
+            tempnumber /=10;
+            busnumber++;
+        }
+
+        tempnumber=number;
+        int result=0;
+
+
+        while(tempnumber !=0){
+
+            basvalue=tempnumber % 10;
+            for(int i=1; i<=busnumber; i++){
+
+            }result +=basvalue;
+                    tempnumber/=10;
+        }System.out.println(result);
+
+
+
+
+    }}
+
+
+
+
+
+
+
+
+//www.patika.dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
